36bb74c557c97950b37fc038f010a0e0f4d4cfff
nio <jean.huaringa@gmail.com>
2021-02-14 13:12:09 -0500

    listas controlador vista

--- src/main/java/com/emp/controller/HomeController.java
+++ src/main/java/com/emp/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.emp.controller;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,23 @@ public class HomeController {
 		return "home";
 	}
 	
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String mostrarPrincipal(Model model) {
+		
+		System.out.println("principal");
+		List<String> peliculas = new LinkedList<>();
+		peliculas.add("Rapido y Furiso");
+		peliculas.add("El Aro 2");
+		peliculas.add("Iron Man");
+		
+		model.addAttribute(peliculas);//"peliculas",
+		
+		return "home";
+	}
+	
 	@RequestMapping(value="/detail")
 	public String mostrarDetalle(Model model) {
+		
 		String tituloPelicula = "Iron Man";
 		int duracion = 130;
 		double precio = 14.5;
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -1,12 +1,20 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>Bienvenido a CineApp</title>
 </head>
 <body>
-	<h1>Hola</h1>
+	<h1>Lista de Peliculas</h1>
+	
+	<ol>
+		<c:forEach items="${peliculas}" var="pelicula">
+			<li><c:out value="${pelicula}"></c:out></li>			
+		</c:forEach>
+	</ol>
+	
 </body>
 </html>
