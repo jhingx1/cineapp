d64e1578440967c2cff45ec41cbced03b6ab8b76
nio <jean.huaringa@gmail.com>
2021-02-14 12:50:39 -0500

    pagina detail con controller

--- src/main/java/com/emp/controller/HomeController.java
+++ src/main/java/com/emp/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.emp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,6 +11,19 @@ public class HomeController {
 	@RequestMapping(value="/home",method=RequestMethod.GET)
 	public String goHome() {
 		return "home";
+	}
+	
+	@RequestMapping(value="/detail")
+	public String mostrarDetalle(Model model) {
+		String tituloPelicula = "Iron Man";
+		int duracion = 130;
+		double precio = 14.5;
+		
+		model.addAttribute("tituloPelicula", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precio);
+		
+		return "detalle";
 	}
 
 }
