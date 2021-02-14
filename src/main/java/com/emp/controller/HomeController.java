7b9e84b32e6f2fcabe5e6481233276711d9e9429
nio <jean.huaringa@gmail.com>
2021-02-14 13:21:42 -0500

    lista de peliculas controlador

--- src/main/java/com/emp/controller/HomeController.java
+++ src/main/java/com/emp/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.emp.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.emp.model.Pelicula;
 
 @Controller
 public class HomeController {
@@ -16,18 +19,73 @@ public class HomeController {
 		return "home";
 	}
 	
+//	@RequestMapping(value="/",method=RequestMethod.GET)
+//	public String mostrarPrincipal(Model model) {
+//		
+//		System.out.println("principal");
+//		List<String> peliculas = new LinkedList<>();
+//		peliculas.add("Rapido y Furiso");
+//		peliculas.add("El Aro 2");
+//		peliculas.add("Iron Man");
+//		
+//		model.addAttribute("peliculas",peliculas);
+//		
+//		return "home";
+//	}
+	
 	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String mostrarPrincipal(Model model) {
 		
 		System.out.println("principal");
-		List<String> peliculas = new LinkedList<>();
-		peliculas.add("Rapido y Furiso");
-		peliculas.add("El Aro 2");
-		peliculas.add("Iron Man");
-		
-		model.addAttribute(peliculas);//"peliculas",
+		List<Pelicula> peliculas = getLista();
+
+		model.addAttribute("peliculas",peliculas);
 		
 		return "home";
+	}
+	
+	private List<Pelicula> getLista(){
+		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+		List<Pelicula> lista = null;
+		try {
+			
+			lista = new LinkedList<>();
+			
+			Pelicula pelicula1 = new Pelicula();
+			pelicula1.setId(1);
+			pelicula1.setTitulo("Power Ranger");
+			pelicula1.setDuracion(120);
+			pelicula1.setClasificacion("B");
+			pelicula1.setGenero("Aventura");
+			pelicula1.setFechaEstreno(formatter.parse("02-05-2017"));
+			
+			Pelicula pelicula2 = new Pelicula();
+			pelicula2.setId(2);
+			pelicula2.setTitulo("La bella y la Bestia");
+			pelicula2.setDuracion(132);
+			pelicula2.setClasificacion("A");
+			pelicula2.setGenero("Infantil");
+			pelicula2.setFechaEstreno(formatter.parse("20-05-2017"));
+			
+			Pelicula pelicula3 = new Pelicula();
+			pelicula3.setId(1);
+			pelicula3.setTitulo("Contratiempo");
+			pelicula3.setDuracion(106);
+			pelicula3.setClasificacion("B");
+			pelicula3.setGenero("Thriller");
+			pelicula3.setFechaEstreno(formatter.parse("28-05-2017"));
+			
+			//agregamos los objetos pelicula a la lista
+			lista.add(pelicula1);
+			lista.add(pelicula2);
+			lista.add(pelicula3);
+			
+			return lista;
+			
+		}catch (Exception e) {
+			System.out.println("Error" + e.getMessage());
+			return null;
+		}
 	}
 	
 	@RequestMapping(value="/detail")
--- webapp/WEB-INF/views/home.jsp
+++ webapp/WEB-INF/views/home.jsp
@@ -5,16 +5,62 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+	crossorigin="anonymous">
+
 <title>Bienvenido a CineApp</title>
 </head>
 <body>
-	<h1>Lista de Peliculas</h1>
+
+<!-- 	<ol> -->
+<%-- 		<c:forEach items="${peliculas}" var="pelicula"> --%>
+<%-- 			<li><c:out value="${pelicula}"></c:out></li>			 --%>
+<%-- 		</c:forEach> --%>
+<!-- 	</ol> -->
 	
-	<ol>
-		<c:forEach items="${peliculas}" var="pelicula">
-			<li><c:out value="${pelicula}"></c:out></li>			
-		</c:forEach>
-	</ol>
+	<div class="container">
+		<div class="row">
+			<h1>Lista de Peliculas</h1>
+			<div class="panel panel-default">
+				<div class="panel-heading">Tabla de Peliculas</div>
+				<div class="panel-body">
+
+					<table class="table table-hover table-striped">
+						<thead>
+							<tr>
+								<td>ID</td>
+								<td>Titulo</td>
+								<td>Duracion</td>
+								<td>Clasifion</td>
+								<td>Genero</td>
+								<td>Fecha Estreno</td>
+								<td>Estatus</td>
+							</tr>
+						</thead>
+						<tbody>
+
+							<c:forEach items="${peliculas}" var="pelicula">
+								<tr>
+									<td>${pelicula.id}</td>
+									<td>${pelicula.titulo}</td>
+									<td>${pelicula.duracion}</td>
+									<td>${pelicula.clasificacion}</td>
+									<td>${pelicula.genero}</td>
+									<td>${pelicula.fechaEstreno}</td>
+									<td>${pelicula.estatus}</td>
+								</tr>
+							</c:forEach>
+
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 	
 </body>
 </html>
